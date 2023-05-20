26ec71dfc61c8a2a538d84a39582fe2d4cb1adf4
Eleyfeson <eleyoliveira@bol.com.br>
2023-05-20 17:59:58 -0300

    Adicionar arquivos de projeto.

---
+++ Factory/ConnectionFactory.cs
@@ -0,0 +1,17 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Factory
+{
+    internal class ConnectionFactory
+    {
+        public IDbConnection GetConnection()
+        {
+            IDbConnection connection = new MySqlConnection()
+            {
+                ConnectionString = "Server=localhost;DataBase=geek_shopping_product_api;Uid=root;Pwd=admin123"
+            };
+            return connection;
+        }
+    }
+}
---
+++ Factory/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Factory;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+ConnectionFactory factory = new ConnectionFactory();
+IDbConnection connection = factory.GetConnection();
+
+connection.Open();
+
+MySqlCommand command = new()
+{
+    Connection = (MySqlConnection)connection,
+    CommandText = "Select name from product",
+};
+
+MySqlDataReader reader = command.ExecuteReader();
+
+while (reader.Read())
+{
+    Console.WriteLine(reader.GetString(0));
+}
+
+connection.Close();
+
